13861a25e9de97d4ca1292f2a201701ccc28552f
Sharmina Akhtar <sakht318@gmail.com>
2018-05-24 04:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  if (distance > 42){
+    return distance - 42;
+  } else {
+    return 42 - distance;
+  }
+}
+
+function distanceFromHqInFeet(feet) {
+  var x = distanceFromHqInBlocks(feet)
+  if (x){
+    return x * 264;
+  }
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    var y = start - end;
+    return y * 264;
+  } else {
+    var x = end - start;
+    return x * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  var x = distanceTravelledInFeet(start, destination);
+  if (x > 400 && x <= 2000){
+    return x * .02;
+  } else if (x <= 400) {
+    return 0;
+  } else if (x > 2000 && x < 2500) {
+    return 25;
+  } else (x > 2500){
+    return 'cannot travel that far';
+  }
+}
