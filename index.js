247538270c2a0fbd383727362a247edafbcdfc75
Sharmina Akhtar <sakht318@gmail.com>
2018-05-24 04:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ function calculatesFarePrice(start, destination) {
   if (x <= 400){
     return 0;
   } else if (x > 400 && x <= 2000) {
-    return .02 * x;
+    return x * .02;
   } else if (x > 2000 && x < 2500) {
     return 25;
-  } else (x > 2500){
+  } else (x > 2500) {
     return 'cannot travel that far';
   }
 }
